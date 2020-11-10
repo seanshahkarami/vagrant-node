f0db5f341dd659133fca76e0ea5eeb119f9493f0
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-11-10 16:51:44 -0600

    added training data dir to nfs

--- provision.sh
+++ provision.sh
@@ -5,6 +5,11 @@ curl -sfL https://get.k3s.io | sh -
 
 # setup nfs
 apt update && apt install -y nfs-kernel-server
-mkdir -p /nfs && chown nobody:nogroup /nfs
+
+mkdir -p /nfs/training-data
+chown -R nobody:nogroup /nfs
+# training-data is write only (users can write / read known file names but list what exists)
+chmod 311 /nfs/training-data
+
 echo '/nfs 10.31.81.0/24(rw,sync,no_subtree_check)' >> /etc/exports
 exportfs -ra
