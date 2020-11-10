02711dc5545efbb126f22bbe7ace004bb6db9f65
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-11-10 16:55:27 -0600

    added training data chmod +r bug reminder

--- provision.sh
+++ provision.sh
@@ -9,6 +9,7 @@ apt update && apt install -y nfs-kernel-server
 mkdir -p /nfs/training-data
 chown -R nobody:nogroup /nfs
 # training-data is write only (users can write / read known file names but list what exists)
+# BUG containers can simply chmod +r and then read everything again
 chmod 311 /nfs/training-data
 
 echo '/nfs 10.31.81.0/24(rw,sync,no_subtree_check)' >> /etc/exports
