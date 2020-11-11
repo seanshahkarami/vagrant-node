d9bf0d03a905a3bd3cb59e27ca5a9396b1f2c4e3
Sean Shahkarami <sean.shahkarami@gmail.com>
2020-11-10 18:31:32 -0600

    /nfs export uses all_squash and anon uid / gid. this seems to lock down the chmod +r security flaw from earlier.

--- provision.sh
+++ provision.sh
@@ -9,8 +9,9 @@ apt update && apt install -y nfs-kernel-server
 mkdir -p /nfs/training-data
 chown -R nobody:nogroup /nfs
 # training-data is write only (users can write / read known file names but list what exists)
-# BUG containers can simply chmod +r and then read everything again
-chmod 311 /nfs/training-data
-
-echo '/nfs 10.31.81.0/24(rw,sync,no_subtree_check)' >> /etc/exports
+chmod 333 /nfs/training-data
+echo '/nfs 10.31.81.0/24(rw,sync,no_subtree_check,all_squash,anonuid=1000,anongid=1000)' >> /etc/exports
 exportfs -ra
+
+# spin up kubernetes nfs volume example
+kubectl create -f /vagrant/examples/nfs-volume
